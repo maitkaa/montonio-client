cb375dae0114c2e20574fa9f22584a75ada8f386
mait.kaasik <mait.kaasik@helmes.com>
2024-03-14 18:24:03 +0200

    finish get payment methods

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,12 +2,14 @@ import axios, { AxiosError, AxiosInstance } from "axios";
 import jwt from "jsonwebtoken";
 
 import { PaymentMethods, PaymentMethodsResponse } from "../types/PaymentMethod";
+import { ERRORS } from "../errors";
 
 interface MontonioClientOptions {
     accessKey: string;
     secretKey: string;
     sandbox?: boolean; // Optional, defaults to true
     url?: string; // Optional, overrides sandbox logic
+    endpoints?: { [key: string]: string }; // Optional, custom endpoints
 }
 
 interface MontonioErrorResponse {
@@ -17,15 +19,28 @@ interface MontonioErrorResponse {
 export class MontonioClient {
     private readonly baseUrl: string;
     private readonly axiosInstance: AxiosInstance;
+    private readonly endpoints: { [key: string]: string };
 
     constructor(options: MontonioClientOptions) {
-        if (!options || !options.accessKey || !options.secretKey) {
-            throw new Error("Missing required options: accessKey and secretKey");
+        if (!options) {
+            throw new Error(ERRORS.MISSING_OPTIONS);
         }
 
-        const { accessKey, secretKey, sandbox, url } = options;
+        const { accessKey, secretKey, sandbox, url, endpoints } = options;
+
+        if (!accessKey) {
+            throw new Error(ERRORS.MISSING_ACCESS_KEY);
+        }
+
+        if (!secretKey) {
+            throw new Error(ERRORS.MISSING_SECRET_KEY);
+        }
 
         this.baseUrl = url ?? (sandbox ? "https://sandbox-stargate.montonio.com" : "https://stargate.montonio.com/");
+
+        this.endpoints = endpoints ?? {
+            getPaymentMethods: "/api/stores/payment-methods",
+        };
 
         const payload = { accessKey };
         const token = jwt.sign(payload, secretKey, { algorithm: "HS256", expiresIn: "10m" });
@@ -40,12 +55,12 @@ export class MontonioClient {
 
     async getPaymentMethods(): Promise<PaymentMethods> {
         try {
-            const response = await this.axiosInstance.get<PaymentMethodsResponse>("/api/stores/payment-methods");
+            const response = await this.axiosInstance.get<PaymentMethodsResponse>(this.endpoints.getPaymentMethods);
             return response.data.paymentMethods;
         } catch (error) {
             if (axios.isAxiosError(error)) {
                 const axiosError = error as AxiosError<MontonioErrorResponse>;
-                throw new Error(`API request failed: ${axiosError.message}`);
+                throw new Error(`${ERRORS.API_REQUEST_FAILED}${axiosError.message}`);
             } else {
                 throw error;
             }
---
+++ src/errors.ts
@@ -0,0 +1,6 @@
+export const ERRORS = {
+    MISSING_OPTIONS: "Missing required parameter: options",
+    MISSING_ACCESS_KEY: "Missing required option: accessKey",
+    MISSING_SECRET_KEY: "Missing required option: secretKey",
+    API_REQUEST_FAILED: "API request failed: "
+};
